cdaab4aed7ce18518126c86fe9f8cd9b055c4ba5
Dmitry Legchikov <legchikov@dmitry.by>
2017-10-26 10:44:04 +0300

    add lexer and interpreter basic tests

--- test/expr_testsuite.py
+++ test/expr_testsuite.py
@@ -1,11 +1,33 @@
-class ExprSuite(object):
+from myself.my_calc import Interpreter, Lexer
+import pytest
 
-    def setUp(self):
 
-    def test_one(self):
-        x = "this"
-        assert 'h' in x
+@pytest.mark.parametrize("text,expected", [
+    ("3+5", 8),
+    ("2+4", 6),
+    ("6*9", 54),
+])
+def test_one_operator(text, expected):
+    assert Interpreter(Lexer(text)).expr() == expected
 
-    def test_two(self):
-        x = "hello"
-        assert hasattr(x, 'check')
+
+@pytest.mark.parametrize("text,expected", [
+    ("3 + 5", 8),
+    (" 2+4", 6),
+    ("6*9 ", 54),
+    (" 3 + 5 ", 8),
+    (" 2+    4", 6),
+    ("   6   *   9   ", 54),
+])
+def test_one_operator_with_space(text, expected):
+    assert Interpreter(Lexer(text)).expr() == expected
+
+
+@pytest.mark.parametrize("text,expected", [
+    ("1+2+3+4", 10),
+    ("1*2*3*4", 24),
+    ("10-20+30-100", -80),
+])
+def test_multi_operator(text, expected):
+    assert Interpreter(Lexer(text)).expr() == expected
+
--- test/lexer_testsuite.py
+++ test/lexer_testsuite.py
@@ -1,8 +1,27 @@
-class TestClass(object):
-    def test_one(self):
-        x = "this"
-        assert 'h' in x
+from myself.my_calc import Lexer, Token, INTEGER, PLUS, MINUS, MUL, DIV
+import pytest
 
-    def test_two(self):
-        x = "hello"
-        assert hasattr(x, 'check')
+
+# @pytest.fixture
+# def create_lexer(test_input):
+#     return Lexer(test_input)
+
+
+@pytest.mark.parametrize("text,expected", [
+    ("3", Token(INTEGER, 3)),
+    ("+", Token(PLUS, '+')),
+    ("/", Token(DIV, '/')),
+])
+def test_single_token(text, expected):
+    assert Lexer(text).get_next_token().value == expected.value
+    # :TODO: compare Token type
+
+
+@pytest.mark.parametrize("text,expected", [
+    ("1+2", [Token(INTEGER, 1), Token(PLUS, '+'), Token(INTEGER, 2)]),
+])
+def test_single_token(text, expected):
+    lexer = Lexer(text)
+    for token in expected:
+        assert lexer.get_next_token().value == token.value
+    # :TODO: compare Token type
